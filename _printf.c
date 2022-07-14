71928267450d03039c56e030cc9b90f9034f3edb
ndiayesamba <ndiayesamba1996@gmail.com>
2022-07-14 13:51:36 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,54 +1,53 @@
-#include <stdarg.h>
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _printf - a remake of the original printf function
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
  *
- * @format: the string to be printed
- *
- * Return: the number of string printed
+ * Return: number of chars printed.
  */
-
 int _printf(const char *format, ...)
 {
-	int i = 0, num = 0;
-	char c, *s;
+unsigned int i = 0, len = 0, ibuf = 0;
+va_list arguments;
+int (*function)(va_list, char *, unsigned int);
+char *buffer;
 
-	va_list(ap);
-
-	va_start(ap, format);
-
-
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			switch (format[i + 1])
-			{
-				case 'c':
-					c = va_arg(ap, int);
-					num += printchar(c);
-					i += 2;
-					break;
-				case 's':
-					s = va_arg(ap, char*);
-					num += printstr(s);
-					i += 2;
-					break;
-				case '%':
-					num += printchar('%');
-					i += 2;
-					break;
-				default:
-					break;
-
-			}
-		}
-		num += printchar(format[i]);
-		i++;
-	}
-
-	va_end(ap);
-	return (num);
+va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+return (-1);
+if (!format[i])
+return (0);
+for (i = 0; format && format[i]; i++)
+{
+if (format[i] == '%')
+{
+if (format[i + 1] == '\0')
+{
+print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+return (-1);
+}
+else
+{
+function = get_print_func(format, i + 1);
+if (function == NULL)
+{
+if (format[i + 1] == ' ' && !format[i + 2])
+return (-1);
+handl_buf(buffer, format[i], ibuf), len++, i--;
+}
+else
+{
+len += function(arguments, buffer, ibuf);
+i += ev_print_func(format, i + 1);
+}
+} i++;
+}
+else
+handl_buf(buffer, format[i], ibuf), len++;
+for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+;
+}
+print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+return (len);
 }
